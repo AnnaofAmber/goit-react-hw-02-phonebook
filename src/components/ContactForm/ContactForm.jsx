6e6f7ae980abf9c6edf9e8b07b7976dd37213c61
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-20 19:06:54 +0300

    completing first step of HW

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,16 @@
-export const ContactForm = props=>{
-    return (
-        <div></div>
-    )
-}
+export const ContactForm = ({ submit, name }) => {
+  return (
+    <form onSubmit={submit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        name={name}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
