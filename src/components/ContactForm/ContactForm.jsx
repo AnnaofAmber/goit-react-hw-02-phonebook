788ee38be98c35645acb18dd10088cfeba21edca
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-20 19:09:38 +0300

    adding number input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-export const ContactForm = ({ submit, name }) => {
+export const ContactForm = ({ submit, name, number }) => {
   return (
     <form onSubmit={submit}>
       <label htmlFor="name">Name</label>
@@ -8,6 +8,15 @@ export const ContactForm = ({ submit, name }) => {
         name={name}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <label htmlFor="number">Number</label>
+      <input
+        id="number"
+        type="tel"
+        name={number}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
       <button type="submit">Add contact</button>
