9d8cd87c89209702c5c9a970d9d0e3012eeaa65b
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-22 21:07:47 +0300

    commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import css from "./ContactForm.module.css"
+
 
 export class ContactForm extends Component {
   state = {
@@ -35,12 +35,12 @@ export class ContactForm extends Component {
 
   render() {
     return (
-      <form className={css.contactForm} onSubmit={this.handleSubmit}>
-        <label className={css.label} htmlFor="name">
+      <form  onSubmit={this.handleSubmit}>
+        <label  htmlFor="name">
           Name
         </label>
         <input
-          className={css.input}
+         
           id="name"
           type="text"
           name="name"
@@ -50,11 +50,11 @@ export class ContactForm extends Component {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
-        <label className={css.label} htmlFor="number">
+        <label  htmlFor="number">
           Number
         </label>
         <input
-          className={css.input}
+          
           id="number"
           type="tel"
           name="number"
@@ -64,7 +64,7 @@ export class ContactForm extends Component {
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
-        <button className={css.btnAdd} type="submit">
+        <button  type="submit">
           Add contact
         </button>
       </form>
