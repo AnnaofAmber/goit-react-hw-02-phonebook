15301acc88de4e27e74aa4c917e14238314447a2
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-22 20:55:34 +0300

    commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import css from './ContactFrom.module.css';
+import css from './ContactForm.module.css';
 
 export class ContactForm extends Component {
   state = {
